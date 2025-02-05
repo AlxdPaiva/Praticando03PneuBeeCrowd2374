0ce0374c4d7905da621798b0f9681c867db45000
Alexandre Paiva <alxd.paiva@outlook.com>
2025-02-04 21:13:00 -0300

    Added program

---
+++ pacoteDownload/Praticando03PneuBeeCrowd2374/src/praticando03pneubeecrowd2374/Praticando03PneuBeeCrowd2374.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praticando03pneubeecrowd2374;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alxdr
+ */
+public class Praticando03PneuBeeCrowd2374 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner teclado = new Scanner(System.in);
+        int N = teclado.nextInt();
+        int M = teclado.nextInt();
+        int dif = N - M;
+        System.out.println(dif);
+    }
+    
+}
